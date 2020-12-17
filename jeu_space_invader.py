7c0576f31bb983c3aab22861a70fbbc531ef5d2d
MoussaELI <moussa.el-idrissi@cpe.fr>
2020-12-17 09:11:17 +0100

    créa fenetre

--- jeu_space_invader.py
+++ jeu_space_invader.py
@@ -1,4 +1,4 @@
-""" lien Git:  """
+""" lien Git: https://github.com/adrevet/TP3-CS-DEV-SPACE-INVADERS.git  """
 
 """
 Date: 17/12/2020
@@ -13,7 +13,21 @@ import random, math
 from tkinter import Tk, PhotoImage, Canvas, Button, Label, StringVar, Entry
 
 """ on créé la fenêtre et on paramètre un canvas : """
-fenetre=Tk()
-fenetre.title("Jeu Space Invader")
 
-canvas=Canvas(fenetre,width=1280,height=720,bg='black')
+
+""" fenetre principale + création canevas : """
+fenetre = Tk()
+fenetre.title('LE JEU DU SPACE INVADERS')
+fenetre.geometry('1280x720')
+
+canevas=Canvas(fenetre,width=1000,height=600,bg='black')
+canevas.pack(side="left",padx=10,pady=10)
+
+boutonQuitter=Button(fenetre,text='Quitter le jeu',fg='red',command=fenetre.destroy)
+boutonQuitter.pack(side='right',padx=90)
+boutonRejouer=Button(fenetre,text='Rejouer',fg='red',command=fenetre.destroy) #faire une fct rejouer
+boutonRejouer.pack(side='right')
+
+
+fenetre.mainloop()
+
