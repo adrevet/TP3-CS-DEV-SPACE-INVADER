c2879401034fcbae372d528dbc601c1cde445543
MoussaELI <moussa.el-idrissi@cpe.fr>
2020-12-17 12:09:23 +0100

    fin de séance 1

--- jeu_space_invader.py
+++ jeu_space_invader.py
@@ -3,14 +3,16 @@
 """
 Date: 17/12/2020
 
-@author: EL IDRISSI Moussa 3ETI
+@author: EL IDRISSI Moussa / DREVET Alexandre 3ETI
 
 Statut:
 """
 
 """ on importe les ressources nécéssaires : """
 import random, math
-from tkinter import Tk, PhotoImage, Canvas, Button, Label, StringVar, Entry, Frame
+from tkinter import Tk, PhotoImage, Canvas, Button, Label, StringVar, Entry, Frame, Menu
+from ma_lib import deplacement, clavier
+from random import uniform
 
 """ on créé la fenêtre et on paramètre un canvas : """
 
@@ -18,21 +20,57 @@ from tkinter import Tk, PhotoImage, Canvas, Button, Label, StringVar, Entry, Fra
 """ fenetre principale + création canevas : """
 fenetre = Tk()
 fenetre.title('LE JEU DU SPACE INVADERS')
-fenetre.geometry('1400x720')
 
-
-
-canevas=Canvas(fenetre,width=1000,height=600,bg='black')
+LARGEUR = 1000
+HAUTEUR = 600
+canevas=Canvas(fenetre,width=LARGEUR,height=HAUTEUR,bg='black')
 boutonQuitter=Button(fenetre,text='Quitter le jeu',fg='red',command=fenetre.destroy)
 boutonRejouer=Button(fenetre,text='Rejouer',fg='red',command=fenetre.destroy) #faire une fct rejouer
 score=Label(fenetre,text='Score : ')
 vies=Label(fenetre,text='Vies : ')
 
-canevas.grid(row=2,column=1,sticky="nesw")
-boutonQuitter.grid(row=2,column=2)
-boutonRejouer.grid(row=2,column=3)
+
+canevas.grid(row=2,column=1,rowspan=4,sticky="nesw")
+boutonQuitter.grid(row=3,column=2,padx=50)
+boutonRejouer.grid(row=4,column=2)
 score.grid(row=1,column=1,sticky="w")
 vies.grid(row=1,column=1,sticky="e")
+
+
+""" menu option avec différentes options : """
+menubar = Menu(fenetre)
+menuoption = Menu(menubar,tearoff =0)
+menuoption.add_command(label="Rejouer", command = fenetre.destroy) #faire une fct rejouer
+menuoption.add_command(label="Quitter", command = fenetre.destroy) # boutton pour arreter de jouer 
+menubar.add_cascade(label="Option", menu = menuoption)
+
+
+""" affichage menu : """
+fenetre.config(menu = menubar)
+
+
+""" création alien : """
+X = LARGEUR/2 #position intiale de l'alien
+Y = HAUTEUR/6 #position intiale de l'alien
+r=25
+
+angle= random.uniform(0,2*math.pi) #direction initiale aléatoire
+vitesse = uniform(1.8,2)*5
+DX=vitesse*math.cos(angle)
+alien = canevas.create_rectangle(X-r,Y-r,X+r,Y+r,width=0,fill="red") #on créé l'aliene
+
+deplacement(X,Y,DX,LARGEUR,r,canevas,alien,fenetre) 
+
+
+""" création vaisseau : """
+PosX = 500 #position intiale du vaisseau
+PosY = 550 #position intiale du vaisseau
+vaisseau = canevas.create_rectangle(PosX-10,PosY-10,PosX+10,PosY+10,width=0,fill="green") #on créé le vaisseau
+canevas.focus_set()
+canevas.bind('<Key>',lambda event:clavier(event,PosX,canevas,vaisseau,PosY))
+
+
+
 
 
 
--- ma_lib.py
+++ ma_lib.py
@@ -1,8 +1,43 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Dec 17 08:27:20 2020
+""" lien Git: https://github.com/adrevet/TP3-CS-DEV-SPACE-INVADERS.git  """
 
-@author: alex
+"""
+Date: 17/12/2020
+
+@author: EL IDRISSI Moussa / DREVET Alexandre 3ETI
+
+Statut:
 """
 
+""" fonction qui décrit le deplacement de l'alien en horizontal : """
+def deplacement(X,Y,DX,LARGEUR,r,canevas,alien,fenetre):
+    
+    if X+r+DX > LARGEUR: #rebond à droite
+        X = 2*(LARGEUR-r)-X
+        DX = -DX
+        
+    if X-r+DX < 0: #rebond à gauche
+        X=2*r-X
+        DX=-DX
+        
+    X = X+DX
+    
+    canevas.coords(alien,X-r,Y-r,X+r,Y+r)
+    fenetre.after(20,lambda:deplacement(X,Y,DX,LARGEUR,r,canevas,alien,fenetre))
+    
+def clavier(event,PosX,canevas,vaisseau,PosY):
+    touche = event.keysym
+    print(touche)
+    DX=0
+    (x1,y1,x2,y2)=canevas.bbox(vaisseau)
+    if touche == "d" and x2 == 991:
+        DX = 0
+    elif touche == "d": #déplacement à droite
+        DX = 10
+    
+    if touche == "q" and x1 == 9:
+        DX = 0
+    elif touche == "q": #déplacement à gauche
+        DX = -10
+    
+    canevas.move(vaisseau,DX,0)
+    
